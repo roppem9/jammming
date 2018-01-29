4d98a8a6d4a97665a0535cd43ba70d8623afdf0e
Elizabeth Ropp <elizmropp@gmail.com>
2018-01-28 21:30:47 -0500

    Feature request & explanation .txt

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './SearchBar.css';
+import { Spotify } from '../../util/Spotify';
 
 export class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {term: ''};
+		
+		this.state = { term: '',
+									 placeholder: 'Search by song, artist, or album',
+									 resultHeader: '' };
+		
 		this.search = this.search.bind(this);
 		this.handleTermChange = this.handleTermChange.bind(this);
 		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.handleTermSubmit = this.handleTermSubmit.bind(this);
 	}
 
 	search() {
@@ -23,16 +29,28 @@ export class SearchBar extends React.Component {
 		if (code === 13) {
 			e.preventDefault();
 			this.search();
+			this.handleTermSubmit();
 		}
 	}
 
+	handleTermSubmit() {
+		this.mainInput.value = '';
+		this.setState({ term: '',
+										placeholder: 'Search for something else',
+		 								resultHeader: `You searched for: "${this.state.term}"`});
+	}
+
 	render() {
+
 		return (
 			<div className="SearchBar">
-  				<input placeholder="Enter a Song Title"
-  					   onChange={this.handleTermChange}
-  					   onKeyPress={this.handleKeyPress} />
-  				<a onClick={this.search}>SEARCH</a>
+  				<h3 className="SearchConfirmation">{this.state.resultHeader}</h3>
+  					<input ref={ (input) => { this.mainInput = input; }}
+  							 	 placeholder={this.state.placeholder}
+  							 	 onChange={this.handleTermChange}
+  							 	 onKeyPress={this.handleKeyPress} />
+  					<button type="submit" onClick={this.handleTermSubmit}
+  				   >SEARCH</button>
 			</div>
 		);
 	}
